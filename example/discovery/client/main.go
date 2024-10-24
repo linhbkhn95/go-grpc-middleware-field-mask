e9246d19ab9137ed25774bcd43faa095fd87a5dd
Trịnh Đức Bảo Linh(Kevin) <trinhducbaolinh@gmail.com>
2024-10-24 11:19:53 +0700

    update example (#5)

--- example/discovery/client/main.go
+++ example/discovery/client/main.go
@@ -19,14 +19,26 @@ func main() {
 		log.Fatal(err)
 	}
 	client := pb.NewDiscoveryServiceClient(conn)
-	req := &pb.ListProductsRequest{
+
+	// for normal response that return all fields
+	normalRequest := &pb.ListProductsRequest{
+		Id: "1"}
+
+	resp, err := client.ListProducts(context.Background(), normalRequest)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("resp for normal request", resp)
+
+	// request that use field mask
+	fieldMaskReq := &pb.ListProductsRequest{
 		Id: "1", FieldMask: &fieldmaskpb.FieldMask{
 			Paths: []string{"result.products.id", "result.products.price"},
 		}}
 
-	resp, err := client.ListProducts(context.Background(), req)
+	resp, err = client.ListProducts(context.Background(), fieldMaskReq)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("resp", resp)
+	fmt.Println("resp with field mask", resp)
 }
